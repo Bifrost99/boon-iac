3e31b28ec1dd18fb95bff371f00b179c655d6e1c
narek.fereshetyan <narek.fereshetyan@onetick.com>
2024-04-18 03:21:49 +0400

    main: added ssh key pair part

--- key.tf
+++ key.tf
@@ -1,6 +1,12 @@
 module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
-
+  source             = "terraform-aws-modules/key-pair/aws"
   key_name           = "boon-key"
   create_private_key = true
 }
+
+resource "null_resource" "write_key" {
+  provisioner "local-exec" {
+    command   = "echo '${module.key_pair.private_key_openssh}' > private_key.pem && echo '${module.key_pair.public_key_openssh}' > boon_key.pub "
+  }
+}
+
